2d6a36edaa157c69e04ca7b2f5bf083b315eef72
Gabriel <glima7591@gmail.com>
2025-03-17 17:28:08 -0300

    adiciona a estrutura do projeto e a pasta `types` para lidar com as interfaces

---
+++ src/types/transaction.ts
@@ -0,0 +1,12 @@
+export interface Transaction {
+    valor: number;
+    dataHora: string;
+}
+
+export interface Statistics {
+  count: number;
+  sum: number;
+  avg: number;
+  min: number;
+  max: number;
+}
